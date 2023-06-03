7b2e2e398a6362385449a1498c9be5eed7551068
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-06-04 00:41:02 +1000

    feat: Create api call logic for place order

---
+++ src/lib/submit-order.ts
@@ -0,0 +1,16 @@
+import { PostOrderResponse, type PostOrder } from '@/backend/order/model';
+import { validatePostOrder } from '@/backend/order/validation';
+
+import { fetcher } from './fetcher';
+
+export default async function submitOrder(body: PostOrder) {
+  const json = await validatePostOrder(body);
+
+  console.log(json);
+
+  return await fetcher
+    .post('/api/order', {
+      json,
+    })
+    .json<PostOrderResponse>();
+}
