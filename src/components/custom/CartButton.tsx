36123e1a22b53e6ff7119a4b5c63cd4a0345c543
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-06-04 17:16:48 +1000

    style: Hide item number on CartButton when there is no cart items

--- src/components/custom/CartButton.tsx
+++ src/components/custom/CartButton.tsx
@@ -1,4 +1,5 @@
 import { ShoppingCartIcon } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
 
 import { useHertzStore } from '@/components/hertz-context';
 
@@ -11,11 +12,18 @@ export default function CartButton({ onClick }: Props) {
 
   return (
     <button
-      className='relative flex w-max flex-col items-center justify-center p-2 text-gray-700 hover:bg-gray-100'
+      className='relative flex w-max flex-col items-center justify-center rounded-lg p-2 text-gray-700 hover:bg-gray-100'
       onClick={onClick}
     >
       <ShoppingCartIcon className='h-6 w-6' />
-      <div className='absolute right-0 -top-1 h-5 w-5 rounded-full bg-red-500 text-white'>
+      <div
+        className={clsx(
+          'absolute right-0 -top-1 h-5 w-5 rounded-full bg-red-500 text-white',
+          {
+            hidden: cartItems.length < 1,
+          }
+        )}
+      >
         <span className='absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 text-xs'>
           {cartItems.length}
         </span>
